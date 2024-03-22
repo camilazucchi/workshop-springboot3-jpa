bb88832cc0928db8cf448fccb8ca763f875ad209
camilazci <camilazucchi@outlook.com>
2024-03-22 15:37:07 -0300

    feat: add findAll and findById methods

--- src/main/java/com/project/course/resources/UserResource.java
+++ src/main/java/com/project/course/resources/UserResource.java
@@ -1,10 +1,16 @@
 package com.project.course.resources;
 
 import com.project.course.entities.User;
+import com.project.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 /* A anotação "@RestController" é uma anotação específica do Spring Framework em Java, geralmente usada em aplicativos
@@ -15,19 +21,24 @@ import org.springframework.web.bind.annotation.RestController;
  * marcada com "@RestController") para uma determinada URL. */
 public class UserResource {
 
-    /* Este método é um exemplo de um controlador em uma aplicação Spring que retorna todos os usuários existentes.
-     * Ele retorna um objeto do tipo "ResponseEntity<User>", isso significa que ele retorna uma resposta HTTP contendo
-     * um objeto do tipo "User".
-     * O trecho "return ResponseEntity.ok().body(user)" é responsável por construir e retornar a resposta HTTP.
-     * - "ResponseEntity.ok()": este método estático da classe "ResponseEntity" cria uma instância de "ResponseEntity"
-     * com o status HTTP 200 OK. Isso indica que a solicitação foi bem-sucedida.
-     * - ".body(user)": define o corpo da resposta com o objeto "user" criado anteriormente. Neste caso, o objeto "User"
-     * será serializado para o corpo da resposta no formato JSON (ou XML, dependendo da configuração). */
+    private final UserService service;
+
+    @Autowired
+    public UserResource(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping
     /* A anotação "@GetMapping" define uma rota que responde a requisições HTTP GET. */
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Maria", "maria@gmail.com", "55 48999325674", "12345");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = service.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<User>> findById(@PathVariable Long id) {
+        Optional<User> object = service.findById(id);
+        return ResponseEntity.ok().body(object);
     }
 
 }
