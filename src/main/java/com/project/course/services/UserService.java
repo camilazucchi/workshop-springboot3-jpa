450c48ff6fac13b21460a7fa26e849db36fbcf8d
camilazci <camilazucchi@outlook.com>
2024-03-22 15:37:22 -0300

    feat: implement UserService class

---
+++ src/main/java/com/project/course/services/UserService.java
@@ -0,0 +1,34 @@
+package com.project.course.services;
+
+import com.project.course.entities.User;
+import com.project.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /* Este método retorna uma lista de todos os usuários presentes no banco de dados. Ele faz isso chamando o método
+     * "findAll()" do "UserRepository", que é uma interface fornecida pelo Spring Data JPA. */
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    /* O tipo "Optional" foi introduzido no Java para lidar de forma mais segura e expressiva com valores que podem
+     * estar ausentes. Ele permite indicar explicitamente que um valor pode estar presente ou ausente, evitando assim
+     * exceções de "NullPointerException". */
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+}
