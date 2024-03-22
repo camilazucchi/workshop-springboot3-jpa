9e9e42e5fb686cb38422e4396dd6ce050b1df2b6
camilazci <camilazucchi@outlook.com>
2024-03-22 13:08:41 -0300

    feat: add UserRepository class to interact with user data

---
+++ src/main/java/com/project/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.project.course.repositories;
+
+import com.project.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/* Esta interface estende "JpaRepository" e é responsável por fornecer métodos para realizar operações de CRUD na
+* entidade "User" do banco de dados. */
+public interface UserRepository extends JpaRepository<User, Long> {
+}
