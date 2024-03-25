c531c550a7bd77d0994b19ca2a2418a6d349a6f0
camilazci <camilazucchi@outlook.com>
2024-03-25 15:54:34 -0300

    feat: add order status to order instances

--- src/main/java/com/project/course/config/TestConfig.java
+++ src/main/java/com/project/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.project.course.config;
 
 import com.project.course.entities.Order;
 import com.project.course.entities.User;
+import com.project.course.enums.OrderStatus;
 import com.project.course.repositories.OrderRepository;
 import com.project.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,9 +41,9 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
         // Aqui estamos fazendo associações passando usuários no terceiro parâmetro:
-        Order o1 = new Order(null, Instant.parse("2024-03-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2024-03-21T03:42:07Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2024-03-22T15:21:07Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2024-03-20T19:53:07Z"), u1, OrderStatus.PAID);
+        Order o2 = new Order(null, Instant.parse("2024-03-21T03:42:07Z"), u2, OrderStatus.DELIVERED);
+        Order o3 = new Order(null, Instant.parse("2024-03-22T15:21:07Z"), u1, OrderStatus.WAITING_PAYMENT);
 
 
         userRepository.saveAll(Arrays.asList(u1, u2));
