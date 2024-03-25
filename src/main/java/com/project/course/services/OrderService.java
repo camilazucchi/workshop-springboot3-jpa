68bb238a071e400a0694618faf211f1beebe70b3
camilazci <camilazucchi@outlook.com>
2024-03-25 13:53:21 -0300

    feat: add order service class

---
+++ src/main/java/com/project/course/services/OrderService.java
@@ -0,0 +1,34 @@
+package com.project.course.services;
+
+import com.project.course.entities.Order;
+import com.project.course.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    /* Este método retorna uma lista de todos os usuários presentes no banco de dados. Ele faz isso chamando o método
+     * "findAll()" do "UserRepository", que é uma interface fornecida pelo Spring Data JPA. */
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    /* O tipo "Optional" foi introduzido no Java para lidar de forma mais segura e expressiva com valores que podem
+     * estar ausentes. Ele permite indicar explicitamente que um valor pode estar presente ou ausente, evitando assim
+     * exceções de "NullPointerException". */
+    public Optional<Order> findById(Long id) {
+        return orderRepository.findById(id);
+    }
+
+}
