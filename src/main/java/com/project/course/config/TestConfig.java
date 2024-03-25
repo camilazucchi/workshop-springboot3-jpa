f0b66aa58cee8a700b1868e55b9bb4cc135fbaa1
camilazci <camilazucchi@outlook.com>
2024-03-25 14:25:11 -0300

    feat: add test to order repository interface, instantiate three order objects and save them to database

--- src/main/java/com/project/course/config/TestConfig.java
+++ src/main/java/com/project/course/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.project.course.config;
 
+import com.project.course.entities.Order;
 import com.project.course.entities.User;
+import com.project.course.repositories.OrderRepository;
 import com.project.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -16,14 +19,16 @@ import java.util.Arrays;
 public class TestConfig implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final OrderRepository orderRepository;
 
     @Autowired
     /* A anotação "@Autowired" é usada para realizar a injeção de dependência automaticamente em classes gerenciadas
      * pelo Spring, como componentes, serviços, repositórios, controladores, etc.
      * Quando usamos essa anotação o Spring procura automaticamente por uma instância correspondente do tipo do campo
      * ou parâmetro e a injeta nesse ponto de injeção. */
-    public TestConfig(UserRepository userRepository) {
+    public TestConfig(UserRepository userRepository, OrderRepository orderRepository) {
         this.userRepository = userRepository;
+        this.orderRepository = orderRepository;
     }
 
     /* Quando a aplicação é iniciada, ela insere dois usuários no banco de dados usando o método "saveAll" do
@@ -34,7 +39,14 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
+        // Aqui estamos fazendo associações passando usuários no terceiro parâmetro:
+        Order o1 = new Order(null, Instant.parse("2024-03-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2024-03-21T03:42:07Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2024-03-22T15:21:07Z"), u1);
+
+
         userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
     }
 
