d754d1b0f0b25bdaa0aa0a8bec0de34ec716feaa
camilazci <camilazucchi@outlook.com>
2024-03-25 15:54:56 -0300

    feat: add order status attribute

--- src/main/java/com/project/course/entities/Order.java
+++ src/main/java/com/project/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.project.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.project.course.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -24,6 +25,7 @@ public class Order implements Serializable {
      * - Pattern: define o padrão para formatar data e hora.
      * - Timezone: especifica o fuso horário que deve ser usado ao serializar/desserializar a data e hora. */
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne
     /* A anotação "@ManyToOne" é usada para mapear um relacionamento muitos-para-um entre duas entidades em um banco
@@ -37,9 +39,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -57,6 +60,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
