34e5b8f0f1e99a8b7059b54e61015753f12b9442
camilazci <camilazucchi@outlook.com>
2024-03-26 14:54:47 -0300

    feat: add product service class

---
+++ src/main/java/com/project/course/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.project.course.services;
+
+import com.project.course.entities.Product;
+import com.project.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+}
