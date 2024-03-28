60cc97c48bbd9cb80de4899ccbc9761d17645fb2
camilazci <camilazucchi@outlook.com>
2024-03-28 14:06:50 -0300

    feat: add Payment entity class

---
+++ src/main/java/com/project/course/entities/Payment.java
@@ -0,0 +1,71 @@
+package com.project.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+    @OneToOne
+    /* A anotação "@OneToOne" é usada para mapear um relacionamento um-para-um entre duas entidades. */
+    @MapsId
+    /* A anotação "@MapsId" é usada para mapear a chave primária de uma entidade filho (ou dependente) com a chave
+     * primária da entidade pai (ou proprietária). Isso é útil quando desejamos compartilhar a mesma chave primária
+     * entre a entidade pai e a entidade filho. */
+    private Order order;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Payment payment = (Payment) object;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
